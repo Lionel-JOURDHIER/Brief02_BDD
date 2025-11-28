dbdda34b463eae698ef1680c2b5e8ddb9c10c04d
Lionel JOURDHIER <lio.jourdhier@gmail.com>
2025-11-28 17:30:55 +0100

    MAJ __init__.py avec Class

--- app/db/tables/__init__.py
+++ app/db/tables/__init__.py
@@ -1,3 +1,31 @@
-"""
-Modules package containing all utilities.
-"""
+from .ADRESSES import Addresses
+from .CITIES import Cities
+from .CITY_PC_CORRESPS import City_pc_corresps
+from .CONTACTS import Contacts
+from .COUNTRIES import Countries
+from .GAMES import Games
+from .GENRES import Genres
+from .PLATEFORMS import Plateforms
+from .PLAYERS import Players
+from .POSTAL_CODES import Postal_codes
+from .PUBLISHERS import Publishers
+from .RELEASE_YEARS import Release_years
+from .REVIEWS import Reviews
+from .STREET_TYPES import Street_types
+
+__all__ =[
+    "Addresses",
+    "Cities",
+    "City_pc_corresps",
+    "Contacts",
+    "Countries",
+    "Games",
+    "Genres",
+    "Plateforms",
+    "Players",
+    "Postal_codes",
+    "Publishers",
+    "Release_years",
+    "Reviews",
+    "Street_types"
+]
