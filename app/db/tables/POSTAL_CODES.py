801d41d07f995839ad0539385770b823bd3b8b34
Lionel JOURDHIER <lio.jourdhier@gmail.com>
2025-11-28 17:13:33 +0100

    Création des Class Postal_codes et Street_types

---
+++ app/db/tables/POSTAL_CODES.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from app.db.base import Base
+
+class Postal_codes(Base):
+    __tablename__ = 'postal_codes'
+    # clé primaire
+    id = Column(Integer, primary_key=True)  
+    postal_code_value = Column(String)
+   
+    # relation 0-N
+    address = relationship("Addresses", back_populates="postal_code")  
---
+++ app/db/tables/STREET_TYPES.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from app.db.base import Base
+
+class Street_types(Base):
+    __tablename__ = 'street_types'
+    # clé primaire
+    id = Column(Integer, primary_key=True)  
+    street_type_name = Column(String)
+   
+    # relation 0-N
+    address = relationship("Addresses", back_populates="street_type")  
