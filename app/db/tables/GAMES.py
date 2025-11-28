08275539843ecf6603a656b32b234c0e718c5781
Lionel JOURDHIER <lio.jourdhier@gmail.com>
2025-11-28 16:53:51 +0100

    correction des FK dans la Class Games

--- app/db/tables/GAMES.py
+++ app/db/tables/GAMES.py
@@ -13,18 +13,18 @@ class Games(Base):
     other_sales = Column(Float(precision=2))
     global_sales =Column(Float(precision=2))
     
-    # clé étrangère Release_years
+    # clé étrangères
     release_year_id = Column(Integer, ForeignKey('release_years.id'))
     release_year = relationship("Release_years", back_populates="game") 
 
     publisher_id = Column(Integer, ForeignKey('publishers.id'))
-    publisher = relationship("Publisher", back_populates="game")
+    publisher = relationship("Publishers", back_populates="game")
 
     plateform_id = Column(Integer, ForeignKey('plateforms.id'))
-    plateform = relationship("Plateform", back_populates="game")  
+    plateform = relationship("Plateforms", back_populates="game")  
 
     genre_id = Column(Integer, ForeignKey('genres.id'))  
     genre = relationship("Genres", back_populates="game")
 
     # relation 0-N
-    reviews = relationship("Reviews", back_populates="game")  
+    review = relationship("Reviews", back_populates="game")  
