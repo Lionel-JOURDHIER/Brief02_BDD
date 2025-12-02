ed719cc867db229a7f04935e3b73748f6c83433f
Lionel JOURDHIER <lio.jourdhier@gmail.com>
2025-12-02 09:05:43 +0100

    Deplacement du fichier session et creation de la fonction create_session

---
+++ app/modules/session.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from db.tables.base import Base
+import db.tables
+
+
+ENGINE = create_engine("sqlite:///ma_base_clients.db", echo=True)
+
+def create_session(engine = ENGINE):
+    '''
+    Create and return a new SQLAlchemy session.
+
+    Args : 
+        engine : sqlalchemy.Engine
+            The SQLAlchemy engine connected to the database.
+
+    Returns : 
+        Session
+            A new SQLAlchemy session instance.
+    '''
+    try : 
+        # create the tables
+        Base.metadata.create_all(engine)
+        # open the session
+        Session = sessionmaker(bind=engine)
+        return Session()
+    except Exception as e:
+        print(f"Error creating database session: {e}")
+
