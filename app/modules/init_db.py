1ff8b966e34af7d88f0dedcc407185991fb8627d
Lionel JOURDHIER <lio.jourdhier@gmail.com>
2025-12-03 13:31:12 +0100

    Mise a jour de la base de donnée après l'exportation

--- app/modules/init_db.py
+++ app/modules/init_db.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from app.modules.insertion_game import insert_game_1, insert_game_2, insert_game_3
-from app.modules.insertion_player import insert_player_1, insert_player_2, insert_player_3, insert_player_4
+from app.modules.insertion_player import insert_player_1, insert_player_2, insert_player_3, insert_player_4, insert_player_5
 from app.modules.insertion_reviews import reviews_to_add
 from app.modules.session import create_session
 
@@ -17,5 +17,6 @@ def insert_all_data(df_players:pd.DataFrame = DF_PLAYERS, df_games:pd.DataFrame 
     insert_player_2(df_players)
     insert_player_3(df_players)
     insert_player_4(df_players)
+    insert_player_5(df_players)
     reviews_to_add(df_reviews)
 
